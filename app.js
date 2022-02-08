1140484ce50e6a879be567495a7a72578c9640e7
Abdirahman Dayib <Abdirahman.Dayib@ga.ntig.se>
2022-02-08 14:17:26 +0100

    added view engine and weekday counter

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req,res) => {
-    console.log('Im working');
-    res.send('Hello Abdirahman!')
-})
+app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+  var currentDay = new Date().getDay();
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var day = days[currentDay];
 
-app.listen(process.env.PORT || 3000 , () => {
-    console.log('Server is running on port 3000');
-})
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
