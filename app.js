d27af48b4d22f84abcd31dd21b4327622f047e92
Abdirahman Dayib <Abdirahman.Dayib@ga.ntig.se>
2022-02-08 12:30:13 +0100

    added procfile and process.env.Port

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req,res) => {
     console.log('Im working');
-    res.sendFile(__dirname + '/index.html')
+    res.send('Hello Abdirahman!')
 })
 
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000 , () => {
     console.log('Server is running on port 3000');
 })
